816f1ef4493cde8ea83989a87dbbf8948a32b38c
haimiko <haimiko@gmail.com>
2018-07-09 07:07:41 -0700

    Update RFIDuino_windows_unlock.ino

--- Firmware/RFIDuino_windows_unlock/RFIDuino_windows_unlock.ino
+++ Firmware/RFIDuino_windows_unlock/RFIDuino_windows_unlock.ino
@@ -53,7 +53,7 @@ void loop()
   }
 
   Serial.print("ID:"); //print a header to the Serial port.
-   for (byte i = 0; i < mfrc522.uid.size+8; i++) 
+   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
       Serial.print(mfrc522.uid.uidByte[i], HEX);
